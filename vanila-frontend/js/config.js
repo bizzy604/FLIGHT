0d9a44e185c6f9d7a5c8319967a636b5ffa64393
Amoni Kevin <kevinyamatta50@gmail.com>
2025-06-05 11:06:54 +0300

    removed unneccessary docs

--- vanila-frontend/js/config.js
+++
@@ -1,7 +0,0 @@
-// Configuration for the frontend application
-const API_BASE_URL = 'http://localhost:5000';
-
-// Export for use in other modules if needed
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { API_BASE_URL };
-}
